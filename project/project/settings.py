3c92d7c850bee1ccbefbdc86dfb87528be8d793d
Artem Korobov <artyom.korobov51@gmail.com>
2023-12-11 23:10:10 +0300

    Change of secret key for hide

--- project/project/settings.py
+++ project/project/settings.py
@@ -131,7 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'artyom.korobov51@gmail.com'
-EMAIL_HOST_PASSWORD = 'fdoflaeynfjmdoxl'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
